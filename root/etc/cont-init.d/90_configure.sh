1b9d3183dbc52a7f89cb61503cba9864feb4c954
Thomas Fanninger <thomas@fanninger.at>
2018-02-18 10:58:50 +0100

    Initial changes

--- root/etc/cont-init.d/90_configure.sh
+++ root/etc/cont-init.d/90_configure.sh
@@ -4,8 +4,6 @@ mkdir -p /config
 mkdir -p /conf
 mkdir -p /download
 
-rm -f /config/aria2.conf
-
 dockerize -template /app/aria2.conf /config/aria2.conf
 dockerize -no-overwrite -template /app/aria2_ext.conf /conf/aria2_ext.conf
 dockerize -no-overwrite -template /app/on-bt-download-complete.sh /conf/on-bt-download-complete.sh
@@ -15,8 +13,6 @@ dockerize -no-overwrite -template /app/on-download-pause.sh /conf/on-download-pa
 dockerize -no-overwrite -template /app/on-download-start.sh /conf/on-download-start.sh
 dockerize -no-overwrite -template /app/on-download-stop.sh /conf/on-download-stop.sh
 
-#cat /conf/aria2_ext.conf >> /config/aria2.conf
-
 chmod +x /conf/on-bt-download-complete.sh
 chmod +x /conf/on-download-complete.sh
 chmod +x /conf/on-download-error.sh
@@ -24,9 +20,15 @@ chmod +x /conf/on-download-pause.sh
 chmod +x /conf/on-download-start.sh
 chmod +x /conf/on-download-stop.sh
 
-#chown -R app:users /download
-#chown -R app:users /config
-#chown -R app:users /conf
+chown -R app:users /download
+chown -R app:users /config
+chown -R app:users /conf
 
-cat /config/aria2.conf
+cat /conf/aria2_ext.conf >> /config/aria2.conf
+
+if [ ! -f /conf/aria2.session ]; then
+	touch /conf/aria2.session
+fi
+
+#cat /config/aria2.conf
 
