19760a3523fabdef846e794ab033f28f03b49f0a
Thomas Fanninger <thomas@fanninger.at>
2018-02-18 10:42:55 +0100

    Initial changes

--- root/etc/cont-init.d/90_configure.sh
+++ root/etc/cont-init.d/90_configure.sh
@@ -4,6 +4,8 @@ mkdir -p /config
 mkdir -p /conf
 mkdir -p /download
 
+rm -f /config/aria2.conf
+
 dockerize -template /app/aria2.conf /config/aria2.conf
 dockerize -no-overwrite -template /app/aria2_ext.conf /conf/aria2_ext.conf
 dockerize -no-overwrite -template /app/on-bt-download-complete.sh /conf/on-bt-download-complete.sh
@@ -13,7 +15,7 @@ dockerize -no-overwrite -template /app/on-download-pause.sh /conf/on-download-pa
 dockerize -no-overwrite -template /app/on-download-start.sh /conf/on-download-start.sh
 dockerize -no-overwrite -template /app/on-download-stop.sh /conf/on-download-stop.sh
 
-cat /conf/aria2_ext.conf >> /config/aria2.conf
+#cat /conf/aria2_ext.conf >> /config/aria2.conf
 
 chmod +x /conf/on-bt-download-complete.sh
 chmod +x /conf/on-download-complete.sh
@@ -22,9 +24,9 @@ chmod +x /conf/on-download-pause.sh
 chmod +x /conf/on-download-start.sh
 chmod +x /conf/on-download-stop.sh
 
-chown -R app:users /download
-chown -R app:users /config
-chown -R app:users /conf
+#chown -R app:users /download
+#chown -R app:users /config
+#chown -R app:users /conf
 
 cat /config/aria2.conf
 
