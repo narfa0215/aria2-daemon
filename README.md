0acb9d0957874c08342f7ed11952f1f64d932767
Thomas Fanninger <thomas@fanninger.at>
2018-02-18 09:06:57 +0100

    Initial changes

--- README.md
+++ README.md
@@ -25,20 +25,3 @@ fanningert/aria2-daemon
 
 ### Link:  
 https://github.com/aria2/aria2  
-
-## User / Group Identifiers
-
-**Note:** (This section stolen from [LinuxServer.io](http://linuxserver.io)'s
-excellent container documentation.)
-
-Sometimes when using data volumes (`-v` flags) permissions issues can arise
-between the host OS and the container. We avoid this issue by allowing you to
-specify the user PUID and group PGID. Ensure the data volume directory on the
-host is owned by the same user you specify and it will "just work" TM.
-
-In this instance `PUID=1001` and `PGID=1001`. To find yours use id user as below:
-
-```
-  $ id <dockeruser>
-    uid=1001(dockeruser) gid=1001(dockergroup) groups=1001(dockergroup)
-```
--- root/etc/cont-init.d/90_configure.sh
+++ root/etc/cont-init.d/90_configure.sh
@@ -1,13 +1,111 @@
 #!/usr/bin/with-contenv bash
 
-dockerize -template /app/start.sh /etc/aria2/start.sh
-dockerize -template /app/aria2.conf /etc/aria2/aria2.conf
-dockerize -template /app/on-bt-download-complete.sh /etc/aria2/config/on-bt-download-complete.sh
-dockerize -template /app/on-download-complete.sh /etc/aria2/config/on-download-complete.sh
-dockerize -template /app/on-download-error.sh /etc/aria2/config/on-download-error.sh
-dockerize -template /app/on-download-pause.sh /etc/aria2/config/on-download-pause.sh
-dockerize -template /app/on-download-start.sh /etc/aria2/config/on-download-start.sh
-dockerize -template /app/on-download-stop.sh /etc/aria2/config/on-download-stop.sh
+rm -f /etc/aria2/aria2.conf
+dockerize -template /app/aria2.conf /app/home/aria2.conf
+dockerize -template /app/on-bt-download-complete.sh /app/home/config/on-bt-download-complete.sh
+dockerize -template /app/on-download-complete.sh /app/home/config/on-download-complete.sh
+dockerize -template /app/on-download-error.sh /app/home/config/on-download-error.sh
+dockerize -template /app/on-download-pause.sh /app/home/config/on-download-pause.sh
+dockerize -template /app/on-download-start.sh /app/home/config/on-download-start.sh
+dockerize -template /app/on-download-stop.sh /app/home/config/on-download-stop.sh
+
+# Create aria2.conf
+if [ $aria2Secret ]; then
+	echo "rpc-secret=${aria2Secret}" >> /app/home/aria2.conf
+fi
+if [ $aria2LogLevel ]; then
+	echo "log-level=${aria2LogLevel}" >> /conf/aria2.conf
+fi
+if [ $aria2MaxConcurrentDownloads ]; then
+	echo "max-concurrent-downloads=${aria2MaxConcurrentDownloads}" >> /app/home/aria2.conf
+fi
+if [ $aria2SeedRatio ]; then
+	echo "seed-ratio=${aria2SeedRatio}" >> /app/home/aria2.conf
+fi
+if [ $aria2SeedTime ]; then
+	echo "seed-time=${aria2SeedTime}" >> /app/home/aria2.conf
+fi
+if [ $aria2DisableIpv6 ]; then
+	echo "disable-ipv6=${aria2DisableIpv6}" >> /app/home/aria2.conf
+fi
+if [ $aria2ForceSave ]; then
+	echo "force-save=${aria2ForceSave}" >> /app/home/aria2.conf
+fi
+if [ $aria2MaxDownloadLimit ]; then
+	echo "max-download-limit=${aria2MaxDownloadLimit}" >> /app/home/aria2.conf
+fi
+if [ $aria2MaxOverallDownloadLimit ]; then
+	echo "max-overall-download-limit=${aria2MaxOverallDownloadLimit}" >> /app/home/aria2.conf
+fi
+if [ $aria2Continue ]; then
+	echo "continue=${aria2Continue}" >> /app/home/aria2.conf
+fi
+if [ $aria2MinSplitSize ]; then
+	echo "min-split-size=${aria2MinSplitSize}" >> /app/home/aria2.conf
+fi
+if [ $aria2Split ]; then
+	echo "split=${aria2Split}" >> /app/home/aria2.conf
+fi
+if [ $aria2MaxConnectionPerServer ]; then
+	echo "max-connection-per-server=${aria2MaxConnectionPerServer}" >> /app/home/aria2.conf
+fi
+if [ $aria2BtRequireCrypto ]; then
+	echo "bt-require-crypto=${aria2BtRequireCrypto}" >> /app/home/aria2.conf
+fi
+if [ $aria2BtMinCryptoLevel ]; then
+	echo "bt-min-crypto-level=${aria2BtMinCryptoLevel}" >> /app/home/aria2.conf
+fi
+if [ $aria2BtSaveMetadata ]; then
+	echo "bt-save-metadata=${aria2BtSaveMetadata}" >> /app/home/aria2.conf
+fi
+if [ $aria2BtLoadSavedMetadata ]; then
+	echo "bt-load-saved-metadata=${aria2BtLoadSavedMetadata}" >> /app/home/aria2.conf
+fi
+if [ $aria2SaveSessionInterval ]; then
+	echo "save-session-interval=${aria2SaveSessionInterval}" >> /app/home/aria2.conf
+fi
+if [ $aria2ListenPort ]; then
+	echo "listen-port=${aria2ListenPort}" >> /app/home/aria2.conf
+fi
+if [ $aria2EnableDht ]; then
+	echo "enable-dht=${aria2EnableDht}" >> /app/home/aria2.conf
+fi
+if [ $aria2DhtListenPort ]; then
+	echo "dht-listen-port=${aria2DhtListenPort}" >> /app/home/aria2.conf
+fi
+
+# Extra aria2 option file
+if [ ! -f /app/home/config/aria2_ext.conf ]; then
+	touch /app/home/config/aria2_ext.conf
+fi
+cat /app/home/config/aria2_ext.conf >> /app/home/aria2.conf
+
+# Create hook files, if not exists
+if [ ! -f /app/home/config/on-bt-download-complete.sh ]; then
+	cp /app/on-bt-download-complete.sh /app/home/config/on-bt-download-complete.sh
+fi
+if [ ! -f /app/home/on-download-complete.sh ]; then
+	cp /app/on-download-complete.sh /app/home/config/on-download-complete.sh
+fi
+if [ ! -f /app/home/on-download-error.sh ]; then
+	cp /app/on-download-error.sh /app/home/config/on-download-error.sh
+fi
+if [ ! -f /app/home/on-download-pause.sh ]; then
+	cp /app/on-download-pause.sh /app/home/config/on-download-pause.sh
+fi
+if [ ! -f /app/home/on-download-start.sh ]; then
+	cp /app/on-download-start.sh /app/home/config/on-download-start.sh
+fi
+if [ ! -f /app/home/on-download-stop.sh ]; then
+	cp /app/on-download-stop.sh /app/home/config/on-download-stop.sh
+fi
+
+if [ ! -f /app/home/aria2.session ]; then
+	touch /app/home/aria2.session
+fi
+
+mkdir -p /home/app/download
+chown -R app:app /home/app
 
 chmod +x /etc/aria2/config/on-bt-download-complete.sh
 chmod +x /etc/aria2/config/on-download-complete.sh
@@ -15,4 +113,6 @@ chmod +x /etc/aria2/config/on-download-error.sh
 chmod +x /etc/aria2/config/on-download-pause.sh
 chmod +x /etc/aria2/config/on-download-start.sh
 chmod +x /etc/aria2/config/on-download-stop.sh
+
+cat /app/home/aria2.conf
 
