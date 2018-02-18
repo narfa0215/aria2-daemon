a3624dc4ad52b7a266695815c3d2f46ba6fde12a
Thomas Fanninger <thomas@fanninger.at>
2018-02-18 10:12:07 +0100

    Initial changes

--- root/etc/cont-init.d/90_configure.sh
+++ root/etc/cont-init.d/90_configure.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/with-contenv bash
 
-mkdir -p /home/app/config
+mkdir -p /config_int
+mkdir -p /config
 
-dockerize -template /app/aria2.conf /home/app/aria2.conf
-dockerize -template /app/on-bt-download-complete.sh /home/app/config/on-bt-download-complete.sh
-dockerize -template /app/on-download-complete.sh /home/app/config/on-download-complete.sh
-dockerize -template /app/on-download-error.sh /home/app/config/on-download-error.sh
-dockerize -template /app/on-download-pause.sh /home/app/config/on-download-pause.sh
-dockerize -template /app/on-download-start.sh /home/app/config/on-download-start.sh
-dockerize -template /app/on-download-stop.sh /home/app/config/on-download-stop.sh
+dockerize -template /app/aria2.conf /config_int/aria2.conf
+dockerize -template /app/on-bt-download-complete.sh /config/on-bt-download-complete.sh
+dockerize -template /app/on-download-complete.sh /config/on-download-complete.sh
+dockerize -template /app/on-download-error.sh /config/on-download-error.sh
+dockerize -template /app/on-download-pause.sh /config/on-download-pause.sh
+dockerize -template /app/on-download-start.sh /config/on-download-start.sh
+dockerize -template /app/on-download-stop.sh /config/on-download-stop.sh
 
 mkdir -p /home/app/download
 
-chmod +x /etc/aria2/config/on-bt-download-complete.sh
-chmod +x /etc/aria2/config/on-download-complete.sh
-chmod +x /etc/aria2/config/on-download-error.sh
-chmod +x /etc/aria2/config/on-download-pause.sh
-chmod +x /etc/aria2/config/on-download-start.sh
-chmod +x /etc/aria2/config/on-download-stop.sh
+chmod +x /config/on-bt-download-complete.sh
+chmod +x /config/on-download-complete.sh
+chmod +x /config/on-download-error.sh
+chmod +x /config/on-download-pause.sh
+chmod +x /config/on-download-start.sh
+chmod +x /config/on-download-stop.sh
 
-chown -R app:users /home/app
+chown -R app:users /config_int
+chown -R app:users /config
 
-cat /app/home/aria2.conf
+cat /config_int/aria2.conf
 
