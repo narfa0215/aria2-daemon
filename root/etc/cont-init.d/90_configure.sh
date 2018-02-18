6d8cdb35f2d1f018fed95e168efc50bed2899fe6
Thomas Fanninger <thomas@fanninger.at>
2018-02-18 11:11:49 +0100

    Initial changes

--- root/etc/cont-init.d/90_configure.sh
+++ root/etc/cont-init.d/90_configure.sh
@@ -4,14 +4,20 @@ mkdir -p /config
 mkdir -p /conf
 mkdir -p /download
 
-dockerize -template /app/aria2.conf /config/aria2.conf
-dockerize -no-overwrite -template /app/aria2_ext.conf /conf/aria2_ext.conf
-dockerize -no-overwrite -template /app/on-bt-download-complete.sh /conf/on-bt-download-complete.sh
-dockerize -no-overwrite -template /app/on-download-complete.sh /conf/on-download-complete.sh
-dockerize -no-overwrite -template /app/on-download-error.sh /conf/on-download-error.sh
-dockerize -no-overwrite -template /app/on-download-pause.sh /conf/on-download-pause.sh
-dockerize -no-overwrite -template /app/on-download-start.sh /conf/on-download-start.sh
-dockerize -no-overwrite -template /app/on-download-stop.sh /conf/on-download-stop.sh
+dockerize -template /app/aria2.conf:/config/aria2.conf
+dockerize -no-overwrite -template /app/aria2_ext.conf:/conf/aria2_ext.conf
+dockerize -no-overwrite -template /app/on-bt-download-complete.sh:/conf/on-bt-download-complete.sh
+dockerize -no-overwrite -template /app/on-download-complete.sh:/conf/on-download-complete.sh
+dockerize -no-overwrite -template /app/on-download-error.sh:/conf/on-download-error.sh
+dockerize -no-overwrite -template /app/on-download-pause.sh:/conf/on-download-pause.sh
+dockerize -no-overwrite -template /app/on-download-start.sh:/conf/on-download-start.sh
+dockerize -no-overwrite -template /app/on-download-stop.sh:/conf/on-download-stop.sh
+
+cat /conf/aria2_ext.conf >> /config/aria2.conf
+
+if [ ! -f /conf/aria2.session ]; then
+	touch /conf/aria2.session
+fi
 
 chmod +x /conf/on-bt-download-complete.sh
 chmod +x /conf/on-download-complete.sh
@@ -23,12 +29,6 @@ chmod +x /conf/on-download-stop.sh
 chown -R app:users /download
 chown -R app:users /config
 chown -R app:users /conf
-
-cat /conf/aria2_ext.conf >> /config/aria2.conf
-
-if [ ! -f /conf/aria2.session ]; then
-	touch /conf/aria2.session
-fi
 
 #cat /config/aria2.conf
 
