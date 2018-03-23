f34ea28e94347df9460d3c59667116b600dc1870
Thomas Fanninger <thomas@fanninger.at>
2018-03-23 22:34:09 +0100

    Addr CURL retry options for RSS downloader

--- root/app/rss_download.sh
+++ root/app/rss_download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while read p; do
-   curl $p | grep -o '<link>.*torrent</link>' | sed -e 's/<[^>]*>//g' | while read line
+   curl --retry 999 --retry-max-time 0 retry-delay 5 $p | grep -o '<link>.*torrent</link>' | sed -e 's/<[^>]*>//g' | while read line
    do
      #echo $line
      ruby /config/aria2rpc.ruby --server 127.0.0.1 --port 6800 --secret YOUR_SECRET_CODE addUri $line
