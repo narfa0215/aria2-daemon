1b820828be0a8a5d73f1f35a891e1b6f9bddb716
Thomas Fanninger <thomas@fanninger.at>
2018-03-24 00:54:53 +0100

    Add rss memory, to prevent another download

--- root/app/rss_download.sh
+++ root/app/rss_download.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
+file=/conf/rss_memory.txt
+
+if [ ! -e "$file" ] ; then
+  touch $file
+fi
+
 while read p; do
-   curl --retry 999 --retry-max-time 0 retry-delay 5 $p | grep -o '<link>.*torrent</link>' | sed -e 's/<[^>]*>//g' | while read line
+   curl --connect-timeout 5 --max-time 10 --retry 999 --retry-max-time 0 retry-delay 5 $p | grep -o '<link>.*torrent</link>' | sed -e 's/<[^>]*>//g' | while read line
    do
      #echo $line
-     ruby /config/aria2rpc.ruby --server 127.0.0.1 --port 6800 --secret {{ default .Env.aria2Secret "YOUR_SECRET_CODE" }} addUri $line
+     if grep -Fxq "$line" $file ; then
+       echo Exist already: $line
+     else
+       ruby /config/aria2rpc.ruby --server 127.0.0.1 --port 6800 --secret {{ default .Env.aria2Secret "YOUR_SECRET_CODE" }} addUri $line
+       echo $line >> $file
+     fi
    done
+   sleep 2
 done </conf/rss_feeds.txt
