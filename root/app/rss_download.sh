56bcd5ec7039dd2196c76275e213a28e3fc38598
Thomas Fanninger <thomas@fanninger.at>
2018-07-15 19:03:34 +0200

    Add option to disable rss line

--- root/app/rss_download.sh
+++ root/app/rss_download.sh
@@ -6,15 +6,19 @@ if [ ! -e "$file" ] ; then
 fi
 
 while read p; do
-   curl $p | grep -o '<link>.*torrent</link>' | sed -e 's/<[^>]*>//g' | while read line
-   do
-     #echo $line
-     if grep -Fxq "$line" $file ; then
-       echo Exist already: $line
-     else
-       ruby /config/aria2rpc.ruby --server 127.0.0.1 --port 6800 --secret {{ default .Env.aria2Secret "YOUR_SECRET_CODE" }} addUri $line
-       echo $line >> $file
-     fi
-   done
-   sleep 2
+  if [[ $p =~ ^#.* ]] ; then
+    echo $p
+  else
+    curl $p | grep -o '<link>.*torrent</link>' | sed -e 's/<[^>]*>//g' | while read line
+    do
+      #echo $line
+      if grep -Fxq "$line" $file ; then
+        echo Exist already: $line
+      else
+        ruby /config/aria2rpc.ruby --server 127.0.0.1 --port 6800 --secret {{ default .Env.aria2Secret "YOUR_SECRET_CODE" }} addUri $line
+        echo $line >> $file
+      fi
+    done
+    sleep 2
+  fi
 done </conf/rss_feeds.txt
